358fb8e56d84a4d395fd213811df8cc0fdc5b876
Daniel Sun <dan.mi.sun@gmail.com>
2014-05-09 16:53:59 +0100

    add before block to author controller for @individual_author method

--- app/controllers/authors.rb
+++ app/controllers/authors.rb
@@ -1,5 +1,9 @@
 PadrinoBlog::App.controllers :authors do
   layout :'main_layout'
+
+  before :show, :edit, :update, :destroy do
+    @individual_author = Author.find(params[:id])
+  end
   
   get :new do
     @author = Author.new
@@ -25,24 +29,20 @@ PadrinoBlog::App.controllers :authors do
   end
 
   get :show, :with => :id do
-    @individual_author = Author.find(params[:id])
     render :'authors/show'
   end
 
   get :edit, :with => :id do
-    @individual_author = Author.find(params[:id])
     render :'authors/edit'
   end
 
   put :update, :with => :id do
-    @individual_author = Author.find(params[:id])
     @individual_author.update_attributes(params[:author])
     redirect url_for(:authors, :index)
 
   end
 
   get :destroy, :with => :id do
-    @individual_author = Author.find(params[:id])
     @individual_author.destroy
     redirect url_for(:authors, :index)
   end
